185b4ccd3f4cb5fc2e1c889cbf1a6da33fb4e66b
Mitali Mehta <mehtm080@newschool.edu>
2016-10-20 03:40:01 +0000

    Adding index file

---
+++ jsintro/scripts.js
@@ -0,0 +1,17 @@
+var age= 17;
+var parentsPresent= false;
+
+if (age >= 18 || parentsPresent == true){
+    console.log("Come on in...");
+} else { 
+    if (age==17 ) {
+    console.log("Come back next year");
+} else {
+    console.log("Come back when you're 18, or bring your parents.");
+}
+}
+
+var myName="Mitali Mehta";
+if (myName = "Mitali Nopany") {
+    console.log("Your name is Mitali Nopany");
+}
