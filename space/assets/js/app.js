496f166a444a86c4df0cfa9beadad16e73d9d511
Mitali Mehta <mehtm080@newschool.edu>
2017-02-16 11:14:59 -0500

    add index file

--- space/assets/js/app.js
+++ space/assets/js/app.js
@@ -71,7 +71,7 @@ $("#swipe2").hover(function() {
 })
 
 
-$('.line1,.line2,.line3,.line4,.line5,.line6,.line7,.line8,.line9,.line10,.line11,.line17').mouseover(function(){
+$('.line1,.line2,.line3,.line4,.line5,.line6,.line7,.line8,.line9,.line10,.line11,.line12,.line13,.line14,.line15,.line16').mouseover(function(){
     $(this).css('background-color','red');
   });
 
